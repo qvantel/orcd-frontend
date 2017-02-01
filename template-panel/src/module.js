e46c534fb4d01f3271976a668087804f20c5d567
root <filip_cross95@hotmail.com>
2017-02-01 12:11:28 +0100

    reverted changes to template plugin

--- template-panel/src/module.js
+++ template-panel/src/module.js
@@ -1,17 +1,10 @@
 import {PanelCtrl} from 'app/features/panel/panel_ctrl';
 import moment from 'moment';
 
-var object = {};
-var pane = [];
 class TestPanelCtrl extends PanelCtrl {
   constructor($scope, $injector) {
     super($scope, $injector);
-    this.object = {
-      msg: "Default Message"
-    };
-    this.time = moment().seconds(0).milliseconds(0).add(1, 'day').toDate();
-
-    this.pane = ["comp", "tel"];
+    this.msg = "Hello there";
   }
 }
 
