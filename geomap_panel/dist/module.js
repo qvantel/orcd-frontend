c0c11918dcf14ca7cd60e04673ede3d390a39d4f
Rasmus Appelqvist <rasmus.a-91@hotmail.com>
2017-02-08 11:49:44 +0100

    Built the plugin with grunt

--- geomap_panel/dist/module.js
+++ geomap_panel/dist/module.js
@@ -1,61 +1,16 @@
 'use strict';
 
-System.register(['app/features/panel/panel_ctrl'], function (_export, _context) {
-  "use strict";
+System.register(['./geomap_ctrl'], function (_export, _context) {
+    "use strict";
 
-  var PanelCtrl, GeoMapPanelCtrl;
-
-  function _classCallCheck(instance, Constructor) {
-    if (!(instance instanceof Constructor)) {
-      throw new TypeError("Cannot call a class as a function");
-    }
-  }
-
-  function _possibleConstructorReturn(self, call) {
-    if (!self) {
-      throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
-    }
-
-    return call && (typeof call === "object" || typeof call === "function") ? call : self;
-  }
-
-  function _inherits(subClass, superClass) {
-    if (typeof superClass !== "function" && superClass !== null) {
-      throw new TypeError("Super expression must either be null or a function, not " + typeof superClass);
-    }
-
-    subClass.prototype = Object.create(superClass && superClass.prototype, {
-      constructor: {
-        value: subClass,
-        enumerable: false,
-        writable: true,
-        configurable: true
-      }
-    });
-    if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;
-  }
-
-  return {
-    setters: [function (_appFeaturesPanelPanel_ctrl) {
-      PanelCtrl = _appFeaturesPanelPanel_ctrl.PanelCtrl;
-    }],
-    execute: function () {
-      _export('PanelCtrl', GeoMapPanelCtrl = function (_PanelCtrl) {
-        _inherits(GeoMapPanelCtrl, _PanelCtrl);
-
-        function GeoMapPanelCtrl($scope, $injector) {
-          _classCallCheck(this, GeoMapPanelCtrl);
-
-          return _possibleConstructorReturn(this, (GeoMapPanelCtrl.__proto__ || Object.getPrototypeOf(GeoMapPanelCtrl)).call(this, $scope, $injector));
+    var GeoMapPanelCtrl;
+    return {
+        setters: [function (_geomap_ctrl) {
+            GeoMapPanelCtrl = _geomap_ctrl.default;
+        }],
+        execute: function () {
+            _export('PanelCtrl', GeoMapPanelCtrl);
         }
-
-        return GeoMapPanelCtrl;
-      }(PanelCtrl));
-
-      GeoMapPanelCtrl.templateUrl = 'module.html';
-
-      _export('PanelCtrl', GeoMapPanelCtrl);
-    }
-  };
+    };
 });
 //# sourceMappingURL=module.js.map
