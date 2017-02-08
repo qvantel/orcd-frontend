aa37b7ac8a6d601fc6045d1a3c0bffb580c5cc40
Rasmus Appelqvist <rasmus.a-91@hotmail.com>
2017-02-08 10:39:57 +0100

    Added constructor to the controller and added functionality to log from scope

--- geomap_panel/src/geomap_ctrl.js
+++ geomap_panel/src/geomap_ctrl.js
@@ -1,6 +1,13 @@
 import {PanelCtrl} from 'app/features/panel/panel_ctrl';
 
 export default class GeoMapPanelCtrl extends PanelCtrl {
+    constructor ($scope, $injector, $log) {
+        super($scope, $injector, $log);
+
+        $scope.log = function (msg) {
+            $log.log(msg);
+        };
+    }
 }
 
 GeoMapPanelCtrl.templateUrl = 'module.html';
