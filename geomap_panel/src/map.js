5394b93c858acebe2028e2b2ed71f5b46ac5f835
Rasmus Appelqvist <rasmus.a-91@hotmail.com>
2017-02-08 10:57:43 +0100

    Loading google geocharts externally and added a class which responsibility is to build the map to be rendered

---
+++ geomap_panel/src/map.js
@@ -0,0 +1,62 @@
+export default class Map {
+    constructor (ctrl, container) {
+        this.ctrl = ctrl;
+        this.container = container;
+
+        this.loadGoogle();
+    }
+
+    loadGoogle () {
+        var self = this;
+
+        if (typeof google === 'undefined') {
+            setTimeout(function () {
+                self.loadGoogle();
+            }, 30);
+        } else {
+            google.charts.load('visualization', '1', {'packages': ['geochart']});
+            google.charts.setOnLoadCallback(function () {
+                self.createMap();
+            });
+        }
+    }
+
+    createMap () {
+        var data = new google.visualization.DataTable();
+
+        data.addColumn('number', 'Lat');
+        data.addColumn('number', 'Long');
+        data.addColumn('number', 'Value');
+
+        var minLat = 55.0;
+        var maxLat = 69.0;
+        var minLong = -15.0;
+        var maxLong = 45.0;
+
+        for (var i = 0; i < 1; i++) {
+            var lat = (Math.random() * (maxLat - minLat) + minLat);
+            var long = (Math.random() * (maxLong - minLong) + minLong);
+
+            lat = Math.round(lat * 100) / 100;
+            long = Math.round(long * 100) / 100;
+
+            data.addRows([[lat, long, 0]]);
+        }
+
+        var options = {
+          region: 'SE',
+          // displayMode: 'markers',
+          // colorAxis: {colors: ['green', 'blue']},
+          backgroundColor: {
+            'fill': '#3a3a3a'
+          },
+          datalessRegionColor: '#151515',
+          legend: 'none',
+          sizeAxis: {minValue: 1, maxValue: 1, minSize: 3, maxSize: 3},
+          colorAxis: {minValue: 0, maxValue: 0, colors: ['#ff0000']}
+        };
+
+        this.map = new google.visualization.GeoChart(this.container);
+        this.map.draw(data, options);
+    }
+}
