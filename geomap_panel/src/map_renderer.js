f35c64ff9bb8b6dda5f48819544e271ada0f359d
Rasmus Appelqvist <rasmus.a-91@hotmail.com>
2017-02-13 13:57:55 +0100

    Now using grafanas render events

--- geomap_panel/src/map_renderer.js
+++ geomap_panel/src/map_renderer.js
@@ -2,16 +2,22 @@ import './css/map.css!';
 import Map from './map';
 
 export default function link (scope, elem, attrs, ctrl) {
-    render();
+    ctrl.events.on('render', function () {
+        render();
+        ctrl.renderingCompleted();
+    });
+
+    ctrl.render();
 
     function render () {
         if (!ctrl.map) {
-            var self = this;
             ctrl.map = new Map(ctrl, elem.find('#map')[0], function () {
                 setTimeout(function () {
-                    self.render();
+                    ctrl.render();
                 }, 5000);
             });
+        } else if (ctrl.map.ready) {
+            ctrl.map.draw();
         }
     }
 }
