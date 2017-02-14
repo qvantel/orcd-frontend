e1bcb3310ce00455fb4c9807645c049b82869d6e
Rasmus Appelqvist <rasmus.a-91@hotmail.com>
2017-02-14 11:20:12 +0100

    The plugin now responds to if the user uses the light or dark theme within Grafana

--- geomap_panel/src/module.js
+++ geomap_panel/src/module.js
@@ -1,4 +1,10 @@
+import {loadPluginCss} from 'app/plugins/sdk';
 import GeoMapPanelCtrl from './geomap_ctrl'
+
+loadPluginCss({
+  dark: 'plugins/qvantel-geomap-panel/css/map.dark.css',
+  light: 'plugins/qvantel-geomap-panel/css/map.light.css'
+});
 
 export {
     GeoMapPanelCtrl as PanelCtrl
