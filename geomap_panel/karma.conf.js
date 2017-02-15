b5d44f4e8c1118a2a1a87abb539f267ae679d8a6
per-lennartsson <perlennartsson@outlook.com>
2017-02-15 09:43:35 +0100

    Fix issue with master

--- geomap_panel/karma.conf.js
+++ geomap_panel/karma.conf.js
@@ -6,9 +6,9 @@ module.exports = (config) => {
     frameworks: ['systemjs', 'mocha', 'expect'],
 
     files: [
-      'node_modules/es6-shim/es6-shim.js',
       'https://www.gstatic.com/charts/loader.js',
       'src/map.js',
+      'src/dataGenerator.js',
       'test/*.js'
     ],
 
