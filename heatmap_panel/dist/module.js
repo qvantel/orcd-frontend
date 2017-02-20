dc560d7998ed34896f8724fb9e52cf545fe5be08
admin <dennis@refoto.se>
2017-02-20 14:29:37 +0100

    remapping import of d3

--- heatmap_panel/dist/module.js
+++ heatmap_panel/dist/module.js
@@ -1,13 +1,13 @@
 'use strict';
 
-System.register(['./heatMapPanelCtrl', './directive.js', './css/vis.min.css!'], function (_export, _context) {
+System.register(['./heatMapPanelCtrl', './directive.js'], function (_export, _context) {
     "use strict";
 
     var HeatMapPanelCtrl;
     return {
         setters: [function (_heatMapPanelCtrl) {
             HeatMapPanelCtrl = _heatMapPanelCtrl.HeatMapPanelCtrl;
-        }, function (_directiveJs) {}, function (_cssVisMinCss) {}],
+        }, function (_directiveJs) {}],
         execute: function () {
             _export('PanelCtrl', HeatMapPanelCtrl);
         }
