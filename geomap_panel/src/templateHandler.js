b03dd24264e8bcb7ded866660b05e3dc47dc5af4
Rasmus Appelqvist <rasmus.a-91@hotmail.com>
2017-03-27 14:38:32 +0200

    Added functionality to the template handler

--- geomap_panel/src/templateHandler.js
+++ geomap_panel/src/templateHandler.js
@@ -8,4 +8,73 @@ export default class TemplateHandler {
         this.templateSrv = templateSrv;
         this.variableSrv = variableSrv;
     }
+
+    addVariable (type, name, label, options, current, query, multi) {
+        if (this.variableExists(name)) return;
+
+        this.variableSrv.addVariable({
+            type: type,
+            name: name,
+            label: label,
+            options: options,
+            current: current,
+            query: query,
+            multi: multi
+        });
+    }
+
+    updateVariable (variableName, options, query, currentText, currentValue) {
+        if (!this.variableExists(variableName)) return;
+
+        var variable = this.variableSrc.variables[this.getVariableIndexByName(variableName)];
+        variable.options = options;
+        variable.query = query;
+        variable.current.text = currentText;
+        variable.current.value = currentValue;
+    }
+
+    variableUpdated () {
+        this.variableSrv.$rootScope.$emit('template-variable-value-updated');
+        this.variableSrv.$rootScope.$broadcast('refresh');
+        this.templateSrv.init(this.variableSrv.variables);
+    }
+
+    deleteVariable () {
+        delete this.countriesVariable;
+        this.variableSrv.variables.splice(1, 1);
+    }
+
+    getVariableIndexByName (name) {
+        if (this.variableSrv.variables) {
+            for (var i = 0; i < this.variableSrv.variables.length; i++) {
+                if (this.variableSrv.variables[i].name === name) {
+                    return i;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    variableExists (name) {
+        return this.getVariableIndexByName(name) !== -1;
+    }
+
+    buildCurrent (text, value) {
+        return {'text': text, 'value': value};
+    }
+
+    buildOption (text, value, selected) {
+        return {'text': text, 'value': value, 'selected': selected};
+    }
+
+    buildOptions (options) {
+        var res = [];
+
+        for (var key in options) {
+            res.push(this.buildOption(options[key].text, options[key].value, options[key].selected));
+        }
+
+        return res;
+    }
 }
