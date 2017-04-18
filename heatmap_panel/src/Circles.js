c21a4dc6d9b1622cbaec97925775b4fd8dac33d1
Kim Svensson Sand <kim.svensson.sand@gmail.com>
2017-04-18 17:26:32 +0200

    Done with names and also fix for getting correct values

--- heatmap_panel/src/Circles.js
+++ heatmap_panel/src/Circles.js
@@ -1,4 +1,5 @@
 import * as d3 from './node_modules/d3/build/d3.min';
+import IndexCalculator from './IndexCalculator';
 
 export default class Circles {
   constructor (ctrl) {
@@ -9,17 +10,16 @@ export default class Circles {
     this.colors = ctrl.panel.colors; // Fix colorbug.
     this.currentColorIndex = 0;
     this.offset = 0;
+    this.indexCalculator = new IndexCalculator();
   }
 
   updateOffset (dataList) {
-    if (dataList[0]) {
-      var tmpOffset = 0;
-      while (!dataList[0].datapoints[dataList[0].datapoints.length - 1 - tmpOffset][0] && tmpOffset < dataList[0].datapoints.length - 1) {
-        tmpOffset++;
-      }
-
-      return tmpOffset;
+    var tmpOffset = 0;
+    while (!dataList[0].datapoints[dataList[0].datapoints.length - 1 - tmpOffset][0] && tmpOffset < dataList[0].datapoints.length - 1) {
+      tmpOffset++;
     }
+
+    return tmpOffset;
   }
 
   getOffset () {
@@ -50,84 +50,85 @@ export default class Circles {
   }
 
   drawCircles (dataList, pointIndex) {
-    this.offset = this.updateOffset(dataList);
-    if (pointIndex === undefined) {
-      this.drawCircles(dataList, dataList[0].datapoints.length - 1 - this.offset);
-    } else {
-      // Set controller this so that it's usable inside d3 function.
-      var classContext = this;
+    // Set controller this so that it's usable inside d3 function.
+    var classContext = this;
 
-      // Select dots div and bind datapoints.
-      var circles = d3.selectAll('#d3-circle-container')
-        .selectAll('svg')
-        .data(dataList);
+    // Select dots div and bind datapoints.
+    var circles = d3.selectAll('#d3-circle-container')
+      .selectAll('svg')
+      .data(dataList);
 
-      // Remove values that are not used.
-      circles.exit().remove();
+    // Remove values that are not used.
+    circles.exit().remove();
 
-      // Add one svg for each circle for new values.
-      circles.enter()
-        .append('svg')
-        .classed('circle-svg', true)
-        .attr('width', this.circleWidth + 40)
-        .attr('height', this.circleWidth + 60)
-        .append('circle') // Append colored circles.
-        .classed('circle', true)
-        .attr('fill', 'white')
-        .attr('cy', (this.circleWidth / 2) + 20)
-        .attr('cx', (this.circleWidth / 2) + 20)
-        .attr('r', function (d, i) {
-          var scale = classContext.getScale(d, i);
-          if (d.datapoints[pointIndex][0]) {
-            return scale(d.datapoints[pointIndex][0]) / 2;
-          } else {
-            return 0;
-          }
-        })
-        .select(function () { // Select parent
-            return this.parentNode;
-        })
-        .append('circle') // Append white circle.
-        .classed('outer-circle', true)
-        .attr('cy', (this.circleWidth / 2) + 20)
-        .attr('cx', (this.circleWidth / 2) + 20)
-        .attr('r', this.circleWidth / 2)
-        .attr('fill-opacity', 0)
-        .attr('stroke-width', 2)
-        .attr('stroke', 'grey');
+    // Add one svg for each circle for new values.
+    circles.enter()
+      .append('svg')
+      .classed('circle-svg', true)
+      .attr('width', this.circleWidth + 40)
+      .attr('height', this.circleWidth + 60)
+      .append('circle') // Append colored circles.
+      .classed('circle', true)
+      .attr('fill', 'white')
+      .attr('cy', (this.circleWidth / 2) + 20)
+      .attr('cx', (this.circleWidth / 2) + 20)
+      .attr('r', function (d, i) {
+        var scale = classContext.getScale(d, i);
+        var index = 0;
+        if (pointIndex) {
+          index = pointIndex;
+        } else {
+          index = classContext.indexCalculator.getLatestPointIndex(d.datapoints);
+        }
 
-      // Update size (and color) of already existing circles.
-      // Have to update color and size in the same function since d3 can't handle concurrent transitions!
-      this.updateCircleSize(dataList, '.circle', pointIndex);
-      // Update service-value.
-      // this.updateTextValue(dataList, '.current-service-value', '');
-    }
+        if (d.datapoints[index][0]) {
+          return scale(d.datapoints[index][0]);
+        } else {
+          return 0;
+        }
+      })
+      .select(function () { // Select parent
+          return this.parentNode;
+      })
+      .append('circle') // Append white circle.
+      .classed('outer-circle', true)
+      .attr('cy', (this.circleWidth / 2) + 20)
+      .attr('cx', (this.circleWidth / 2) + 20)
+      .attr('r', this.circleWidth / 2)
+      .attr('fill-opacity', 0)
+      .attr('stroke-width', 2)
+      .attr('stroke', 'grey');
+
+    // Update size (and color) of already existing circles.
+    this.updateCircleSize(dataList, '.circle', pointIndex);
   }
 
   updateCircleSize (dataList, circleClass, pointIndex) {
-    this.offset = this.updateOffset(dataList);
-    if (pointIndex === undefined) {
-      this.updateCircleSize(dataList, circleClass, dataList[0].datapoints.length - 1 - this.offset);
-    } else {
-      var classContext = this;
+    var classContext = this;
 
-      // Update circle size.
-      d3.selectAll('.circle-svg')
-        .data(dataList)
-        .select(circleClass)
-        .transition()
-        .duration(1000)
-        .attr('cy', (this.circleWidth / 2) + 20)
-        .attr('cx', (this.circleWidth / 2) + 20)
-        .attr('r', function (d, i) {
-          var scale = classContext.getScale(d, i);
-          if (d.datapoints[pointIndex][0]) {
-            return scale(d.datapoints[pointIndex][0]) / 2;
-          } else {
-            return 0;
-          }
-        });
-    }
+    // Update circle size.
+    d3.selectAll('.circle-svg')
+      .data(dataList)
+      .select(circleClass)
+      .transition()
+      .duration(1000)
+      .attr('cy', (this.circleWidth / 2) + 20)
+      .attr('cx', (this.circleWidth / 2) + 20)
+      .attr('r', function (d, i) {
+        var scale = classContext.getScale(d, i);
+        var index = 0;
+        if (pointIndex) {
+          index = pointIndex;
+        } else {
+          index = classContext.indexCalculator.getLatestPointIndex(d.datapoints);
+        }
+
+        if (d.datapoints[index][0]) {
+          return scale(d.datapoints[index][0]);
+        } else {
+          return 0;
+        }
+      });
   }
 
   setCircleColor (dataList, index, circleClass, color) {
