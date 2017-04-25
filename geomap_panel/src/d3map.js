9d9011de354d65180b9c97d954e8a131832e3f6a
per-lennartsson <perlennartsson@outlook.com>
2017-04-25 08:56:42 +0200

    Add debug option to auto select countries

--- geomap_panel/src/d3map.js
+++ geomap_panel/src/d3map.js
@@ -7,6 +7,7 @@ export default class D3map {
         this.container = container;
         this.readyCallback = onReadyCallback;
         this.colorScale = null;
+        this.debug = false;
         this.strokeColors = ['#7EB26D', '#EAB839', '#6ED0E0', '#EF843C', '#E24D42', '#1F78C1', '#BA43A9', '#705DA0', '#508642', '#CCA300', '#447EBC', '#C15C17', '#890F02', '#0A437C', '#6D1F62', '#584477', '#B7DBAB', '#F4D598',
         '#70DBED', '#F9BA8F', '#F29191', '#82B5D8', '#E5A8E2', '#AEA2E0', '#629E51', '#E5AC0E', '#64B0C8', '#E0752D', '#BF1B00', '#0A50A1', '#962D82', '#614D93', '#9AC48A', '#F2C96D', '#65C5DB', '#F9934E', '#EA6460', '#5195CE',
         '#D683CE', '#806EB7', '#3F6833', '#967302', '#2F575E', '#99440A', '#58140C', '#052B51', '#511749', '#3F2B5B', '#E0F9D7', '#FCEACA', '#CFFAFF', '#F9E2D2', '#FCE2DE', '#BADFF4', '#F9D9F9', '#DEDAF7'];
@@ -132,9 +133,9 @@ export default class D3map {
             .attr('d', path.pointRadius(20.0 / xyz[2]));
         }
 
-        function countryClicked (d) {
+        function countryClicked (d, debug) {
             if (typeof d !== 'undefined') {
-                if (self.ctrl.inputHandler.isCtrlDown() || self.ctrl.inputHandler.isShiftDown()) {
+                if (self.ctrl.inputHandler.isCtrlDown() || self.ctrl.inputHandler.isShiftDown() || debug) {
                     self.ctrl.selectedCountriesHandler.onCountryClicked(d.id);
                     /*
                     if (self.ctrl.selectedCountriesHandler.isCountrySelected(d.id) !== -1) {
@@ -159,6 +160,15 @@ export default class D3map {
                 }
             }
         }
+
+        if (this.debug) {
+            d3.json('public/plugins/qvantel-geomap-panel/data/countries.json', function (world) {
+                for (var i = 0; i < 180; i++) {
+                    self.ctrl.log(world.objects.countries.geometries[i]);
+                    countryClicked(world.objects.countries.geometries[i], true)
+                }
+            });
+        }
         this.updateStrokeColor();
         this.sortCountries();
     }
@@ -181,6 +191,7 @@ export default class D3map {
 
         var colorIndex = 0;
         for (var i = 0; i < countries.length; i++) {
+            this.ctrl.log(d3.select('#' + countries[i].toUpperCase()) + ' ' + i + ' ' + colorIndex)
             d3.select('#' + countries[i].toUpperCase())
             .classed('stroke-selected', true)
             .style('stroke', this.strokeColors[colorIndex]);
