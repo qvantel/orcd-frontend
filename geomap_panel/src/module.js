d82865c08a06e55d13616e2c0f4afa2420966511
per-lennartsson <perlennartsson@outlook.com>
2017-05-09 12:07:04 +0200

    update db for grafana. Clean up repo for delivery

--- geomap_panel/src/module.js
+++ geomap_panel/src/module.js
@@ -5,8 +5,8 @@ import GeoMapPanelCtrl from './geomap_ctrl'
 * Set which stylesheets to be used depending on the Grafana theme used
 */
 loadPluginCss({
-    dark: 'plugins/qvantel-geomap-panel/css/map.dark.css',
-    light: 'plugins/qvantel-geomap-panel/css/map.light.css'
+    dark: 'plugins/geomap-panel/css/map.dark.css',
+    light: 'plugins/geomap-panel/css/map.light.css'
 });
 
 export {
